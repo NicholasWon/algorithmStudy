4210e766f2da4bfce4c1446e287ec04dd21da0c3
Nicholas <franwon@naver.com>
2017-08-25 16:48:39 +0900

    Update Solve_Me_First.swift

--- HackerRank/Solve_Me_First.swift
+++ HackerRank/Solve_Me_First.swift
@@ -1,20 +1,6 @@
-import Foundation
+// read integers line by line
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
 
-let n = Int(readLine()!)!
-
-let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
-let max = input.max()!
-
-func getGcd(_ a: Int, _ b: Int) -> Int { // Greast Common Dividsor
-    return (a % b == 0) ? b : getGcd(b, a % b) 
-}
-
-func primeNumbers(lessThan i: Int) -> [Int] {
-//    return zip(Array(1...i),Array(1...i)).map { getGcd($0, $1) }
-    let inputs = Array(1...i)
-    let result = Array(1...i).map { (k: Int) in inputs.map { getGcd($0, k) } }
-    print(result)
-    return []
-}
-
-print(primeNumbers(lessThan: max))
+// Hint: Type print(a + b) below
+print(a + b)
